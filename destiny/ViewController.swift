f355d36ac40521897d943c6bf70b9bcb8960347a
mikedilulo <mikedilulo2020@gmail.com>
2020-04-18 14:43:39 -0600

    Initial Commit

---
+++ destiny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  destiny
+//
+//  Created by Mike Di’ Lulo on 4/18/20.
+//  Copyright © 2020 Mike Di’ Lulo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
