2dc5b387981859954e2324f5babd1d367d3c5a98
mikedilulo <mikedilulo2020@gmail.com>
2020-04-18 14:57:24 -0600

    Add IBOutlet for Choice One Button

--- destiny/ViewController.swift
+++ destiny/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choiceOneButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
